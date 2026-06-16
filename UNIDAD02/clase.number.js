3b23009984dd937d0a3a9eb14d4b350e7eb57c34
JohanelENP <johanel.castillo@unl.edu.ec>
2026-06-16 12:34:17 -0500

    Add files via upload

---
+++ UNIDAD02/clase.number.js
@@ -0,0 +1,28 @@
+
+
+
+const cientifico = 5e3
+const infinito = Infinity
+const noEsUnNuemero = NaN
+
+///SUMAS RESTAS
+ const suma = 3 + 4
+ const resta = 4 - 4
+ const multiplicacion = 4 * 7
+ const division = 10/2
+
+//MODULOS EXPONENCIACION
+ const modulo = 15 % 8
+ const exponenciacion = 0.1 + 0.2
+
+//RESULTADO DE PRECISION
+const exponenciacion = 0.1 + 0.2
+console.log(exponenciacion)
+console.log(exponenciacion.toFixed(1))
+
+const raizcuadrada = Math.sqrt(16)
+const valorAbsoluto = Math.abs(-7)
+const aleatorio = Math.random()
+console.log(raizcuadrada)
+console.logv(valorAbsoluto)
+console.log(aleatorio)
